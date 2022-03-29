0cc1fe301e83e1aea0675b354a2fee9bd33af9dd
Ilya Isko <ilufkai2000@gmail.com>
2022-03-30 03:56:28 +0700

    pipeline: season features transformer: add logic, rewrite _set_target() function

--- data_transformer.py
+++ data_transformer.py
@@ -4,6 +4,17 @@ from sklearn.model_selection import train_test_split
 from typing import List, Dict, Tuple, NoReturn
 
 
+def _set_target(row) -> NoReturn:
+    """ Set target feature from score """
+
+    if row.home_scored > row.away_scored:
+        return 3
+    elif row.home_scored == row.away_scored:
+        return 1
+    else:
+        return 0
+
+
 class DataTransformer:
 
     def __init__(self, context: Dict):
@@ -15,7 +26,8 @@ class DataTransformer:
 
     def run_logic(self):
         self._fill_nans()
-        self._set_target()
+        self.train_data['target'] = self.train_data.apply(_set_target, axis=1)
+        self.test_data['target'] = self.test_data.apply(_set_target, axis=1)
         self._team_encoding()
         self._categorical_encoding()
         self._generate_features()
@@ -31,11 +43,16 @@ class DataTransformer:
     def _split(self) -> NoReturn:
         pass
 
-    def _set_target(self) -> NoReturn:
-        pass
-
     def _team_encoding(self) -> NoReturn:
-        pass
+
+        all_teams = self.train_data.sort_values(by='home_team').home_team.unique()
+        self.teams_labels = {team: number for number, team in enumerate(all_teams)}
+        self.decode_teams = {number: team for number, team in enumerate(all_teams)}
+
+        self.train_data['home_team'] = self.train_data['home_team'].map(self.teams_labels)
+        self.train_data['away_team'] = self.train_data['away_team'].map(self.teams_labels)
+        self.test_data['home_team'] = self.test_data['home_team'].map(self.teams_labels)
+        self.test_data['away_team'] = self.test_data['away_team'].map(self.teams_labels)
 
     def _generate_features(self) -> NoReturn:
         self._season_averages()
