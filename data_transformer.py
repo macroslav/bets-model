c2215a7a8144ccfa95865241fec4f6be071efed6
akrjuchkov <al.krjuchkov@cft.ru>
2022-07-31 18:22:51 +0700

    Правки

--- data_transformer.py
+++ data_transformer.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 from typing import List, Dict, Tuple, NoReturn
